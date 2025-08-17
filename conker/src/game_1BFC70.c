5f27e1eacf4e7ba6b874a53dca3d26f0218e6da1
NameSpace-64 <Orrin.lewdcac64@gmail.com>
2025-08-17 14:00:13 -0400

    Update game_1BFC70.c
    credit: inspectredc

--- conker/src/game_1BFC70.c
+++ conker/src/game_1BFC70.c
@@ -3,13 +3,12 @@
 #include "variables.h"
 
 
-#pragma GLOBAL_ASM("asm/nonmatchings/game_1BFC70/func_151927C0.s")
-// JUSTREG: using t2 not t3
-// void func_151927C0(struct114 *arg0) {
-//     s32 tmp0;
-//     tmp0 = (s16)((arg0->unk14 & 0xFF) >> 1);
-//     arg0->unk38 = 300;
-//     arg0->unk3A = 10;
-//     arg0->unk14 = tmp0 * 65537;
-//     arg0->unk3B = 0;
-// }
+
+void func_151927C0(struct114* arg0) {
+    s16 temp = (arg0->unk14 & 0xFF) >> 1;
+
+    arg0->unk38 = 0x12C;
+    arg0->unk3A = 0xA;
+    arg0->unk14 = (temp << 16) + temp;
+    arg0->unk3B = 0;
+}
